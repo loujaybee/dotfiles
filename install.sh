b00eb5e452e1f9505b016ee6f59fc121fd1ad8c0
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-04-22 18:08:02 +0000

    fix: convert to fish

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "hello world!";
 sudo apt install fonts-firacode
 sudo apt-get install fish
+fish
 curl -L https://get.oh-my.fish > install
 fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf
 omf install agnoster
