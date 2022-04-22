ceef6a81c176f6ccd7ec0e92dd6d99881f426ef0
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-04-22 17:24:21 +0000

    fix: update dotfiles

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 echo "hello world!";
 sudo apt install fonts-firacode
 sudo apt-get install fish
+curl -L https://get.oh-my.fish > install
+fish install --path=~/.local/share/omf --config=~/.config/omf
+omf install agnoster
