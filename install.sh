1e1add591146254fdafca030c87b48e0a49825aa
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-04-22 17:31:49 +0000

    fix: updated shell

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ echo "hello world!";
 sudo apt install fonts-firacode
 sudo apt-get install fish
 curl -L https://get.oh-my.fish > install
-fish install --path=~/.local/share/omf --config=~/.config/omf
+fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf
 omf install agnoster
