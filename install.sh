261a4bf17bddc8146bf08ab1a18bef4993eeb589
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-04-27 10:26:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ sudo apt-get install fish
 curl -L https://get.oh-my.fish > install
 fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf
 /usr/bin/fish -c "omf install agnoster"
+SHELL=/bin/fish
