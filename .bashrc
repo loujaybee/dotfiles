7a331e6268893b433ac1f4fe02d9d9ca2e49a719
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-09-15 06:12:07 +0000

    fix: add bashrc

---
+++ .bashrc
@@ -0,0 +1,2 @@
+#!/bin/bash
+alias lou="echo test"
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "hello world!";
-sudo apt install fonts-firacode
 sudo apt-get install fish
 curl -L https://get.oh-my.fish > install
 fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf
 /usr/bin/fish -c "omf install agnoster"
 SHELL=/bin/fish
+./.bashrc
