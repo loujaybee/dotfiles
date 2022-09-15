4a2a6899c63e2d88ce8d36b8d9bfffba45e07771
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-09-15 06:16:10 +0000

    fix: add sudo

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 echo "hello world!";
+sudo ./.bashrc
 sudo apt-get install fish
 curl -L https://get.oh-my.fish > install
 fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf
 /usr/bin/fish -c "omf install agnoster"
 SHELL=/bin/fish
-./.bashrc
