29b3b753726b12ffa8b8cc2eb9d05f4a79e335a6
loujaybee <louis@gitpod.io>
2023-06-01 15:17:04 +0000

    fix: updated dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 # Install Fish
 sudo apt-get install fish
+echo "apt install fish finished"
 curl -L https://get.oh-my.fish > install
+echo "Install oh my fish"
 fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf
+echo "Run fish install"
 /usr/bin/fish -c "omf install agnoster"
+echo "omf finished"
 SHELL=/bin/fish
+echo "Shell set to fish"
+
