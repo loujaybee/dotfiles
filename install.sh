b9675a148b580af4a9e2cd726885b4e776071d4d
loujaybee <louis@gitpod.io>
2023-06-01 15:57:34 +0000

    fix: add tmux

--- install.sh
+++ install.sh
@@ -1,15 +1,29 @@
 # Install Fish
+
+echo "Install fish started"
 sudo apt-get install fish
+
 echo "apt install fish finished"
 curl -L https://get.oh-my.fish > install
+
 echo "Install oh my fish"
 fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf --noninteractive
+
 echo "Run fish install"
 /usr/bin/fish -c "omf install agnoster"
+
 echo "omf finished"
 SHELL=/bin/fish
+
 echo "Shell set to fish"
 
 # Make fish start in bash shells by default
 # echo "SHELL=/bin/fish" >> ~/.bashrc
 # echo "exec /bin/fish -l" >> ~/.bashrc
+
+echo "Start installing tmux"
+
+sudo apt-get update
+sudo apt-get install tmux
+
+echo "End installing tmux"
