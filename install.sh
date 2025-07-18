bda403dcb5f7743f8fc14c34706ec2a66222c7fa
Lou Bichard <louis@gitpod.io>
2025-07-18 10:45:45 +0000

    comment out fish and atuin, add cc

--- install.sh
+++ install.sh
@@ -9,40 +9,40 @@ sudo apt-get update --noninteractive
 
 # Install Fish
 
-echo "Install fish started"
-sudo apt-get install fish
+# echo "Install fish started"
+# sudo apt-get install fish
 
-echo "apt install fish finished"
-curl -L https://get.oh-my.fish > install
+# echo "apt install fish finished"
+# curl -L https://get.oh-my.fish > install
 
-echo "Install oh my fish"
-fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf --noninteractive
+# echo "Install oh my fish"
+# fish install --path=/home/gitpod/.local/share/omf --config=~/.config/omf --noninteractive
 
-echo "Run fish install"
-/usr/bin/fish -c "omf install agnoster"
+# echo "Run fish install"
+# /usr/bin/fish -c "omf install agnoster"
 
-echo "omf finished"
-export SHELL=/bin/fish
+# echo "omf finished"
+# export SHELL=/bin/fish
 
-echo "Move shell alias folder"
-mv config.fish ~/.config/fish/config.fish
+# echo "Move shell alias folder"
+# mv config.fish ~/.config/fish/config.fish
 
-echo "Shell set to fish"
+# echo "Shell set to fish"
 
 # Make fish start in bash shells by default
 # echo "SHELL=/bin/fish" >> ~/.bashrc
 # echo "exec /bin/fish -l" >> ~/.bashrc
 
-echo "Start installing libutempter0, dependency of tmux to avoid interactive prompt"
-sudo apt-get install libutempter0
+# echo "Start installing libutempter0, dependency of tmux to avoid interactive prompt"
+# sudo apt-get install libutempter0
 
-echo "Start installing tmux"
-sudo apt-get install tmux
+# echo "Start installing tmux"
+# sudo apt-get install tmux
 
-echo "Install teamocil"
-gem install teamocil
+# echo "Install teamocil"
+# gem install teamocil
 
-echo "End installing tmux"
+# echo "End installing tmux"
 
 # echo "Install and setup atuin"
 
@@ -54,3 +54,7 @@ echo "End installing tmux"
 # atuin sync
 
 # echo "End installing atuin"
+
+echo "Installing Claude Code..."
+./claude/.run
+
